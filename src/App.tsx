1ba3bf1dd4e286c59917dfcf095ee1a14368631c
heowen <heowen@udel.edu>
2024-04-04 12:27:10 -0400

    added packages for multi page website

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, Form } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
