d67d51ec5cbed2b5a4ec0913b09cbd85d8d0c9d5
Kevin Kramer <kevinjkramer168@gmail.com>
2024-04-10 17:29:57 -0400

    fixed more issues that came from merging

--- src/components/buttons/ToDetailedButton.tsx
+++ src/components/buttons/ToDetailedButton.tsx
@@ -1,39 +1,44 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import { Routes, useNavigate } from 'react-router-dom';
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 interface toDetailedButtonProps {
-    isHomeButtonClicked: boolean;
-    setIsHomeButtonClicked: (isHomeButtonClicked: boolean) => void;
-    isQuizButtonClicked: boolean;
-    setIsQuizButtonClicked: (isQuizButtonClicked: boolean) => void;
+  isHomeButtonClicked: boolean;
+  setIsHomeButtonClicked: (isHomeButtonClicked: boolean) => void;
+  isQuizButtonClicked: boolean;
+  setIsQuizButtonClicked: (isQuizButtonClicked: boolean) => void;
 }
 
-function ToDetailedButton({ isQuizButtonClicked, setIsQuizButtonClicked, isHomeButtonClicked, setIsHomeButtonClicked }: toDetailedButtonProps) {
-    let navigate = useNavigate();
-    const routeChange = () => {
-        let path = '/Detailed';
-        navigate(path);
-    }
+function ToDetailedButton({
+  isQuizButtonClicked,
+  setIsQuizButtonClicked,
+  isHomeButtonClicked,
+  setIsHomeButtonClicked,
+}: toDetailedButtonProps) {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = "/Detailed";
+    navigate(path);
+  };
 
-    function handleQuizButtonClick() {
-        setIsHomeButtonClicked(!isHomeButtonClicked);
-        setIsQuizButtonClicked(!isQuizButtonClicked);
-        routeChange();
-    };
-  
+  function handleQuizButtonClick() {
+    setIsHomeButtonClicked(!isHomeButtonClicked);
+    setIsQuizButtonClicked(!isQuizButtonClicked);
+    routeChange();
+  }
 
-
-    return (
-        <Button 
-        variant = "secondary" 
-        onClick = { handleQuizButtonClick }    
-         style={{ 
-            padding: '20px', 
-            fontSize: '20px'
-          }}> Detailed Quiz</Button >
-
-    );
-};
+  return (
+    <Button
+      variant="secondary"
+      onClick={handleQuizButtonClick}
+      style={{
+        padding: "20px",
+        fontSize: "20px",
+      }}
+    >
+      {" "}
+      Detailed Quiz
+    </Button>
+  );
+}
 
 export default ToDetailedButton;
