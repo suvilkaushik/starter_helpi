cc6e2e555883f9fc1c293a1d74691ebfb8f0b766
SUVIL KAUSHIK <suvilk@udel.edu>
2024-05-06 18:30:58 -0400

    added formatting to results page

--- src/components/pages/Results.tsx
+++ src/components/pages/Results.tsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import "../../App.css";
 
 function Results({
   returnValue,
   setReturnValue,
 }: {
-  returnValue: any;
+  returnValue: string;
   setReturnValue: any;
 }) {
-  //   return <></>;
+  let jsonData = JSON.parse(returnValue);
+  const [careerData, setCareerData] = useState<{
+    [key: string]: string;
+  } | null>(null);
+  useEffect(() => {
+    const parsedData = JSON.parse(jsonData);
+    setCareerData(parsedData);
+  }, [jsonData]);
+
   return (
     <div className="questionContainer">
-      <p>{returnValue}</p>;
+      {" "}
+      {careerData && (
+        <>
+          <h2>{careerData.career}</h2>
+          <p>{careerData.potential_job_title}</p>
+          <p>{careerData.reasoning}</p>
+        </>
+      )}
     </div>
   );
 }
